f9c1e375b9495ab046fe61baf2fbe0d6f503b563
Gusb3ll <Kitpipat.0647@gmail.com>
2021-04-26 11:24:03 +0700

    Added findinput.py

---
+++ ext/findinput.py
@@ -0,0 +1,12 @@
+import pyaudio
+
+p = pyaudio.PyAudio()
+
+for i in range(p.get_device_count()):
+    print(p.get_device_info_by_index(i))
+
+for i in range(p.get_device_count()):
+    dev = p.get_device_info_by_index(i)
+    if (dev['name'] == 'Stereo Mix (Realtek(R) Audio)' and dev['hostApi'] == 0):
+        dev_index = dev['index']
+        print('dev_index', dev_index)
