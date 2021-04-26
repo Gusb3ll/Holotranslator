f629840a4e014fa9e4ec1f57c437e65f68f03f9b
Gusb3ll <Kitpipat.0647@gmail.com>
2021-04-27 01:19:22 +0700

    Make findinput.py only find SteroMix input index

--- ext/findinput.py
+++ ext/findinput.py
@@ -3,12 +3,13 @@ import pyaudio, time
 p = pyaudio.PyAudio()
 
 for i in range(p.get_device_count()):
-    print(p.get_device_info_by_index(i))
+    dev = p.get_device_info_by_index(i)
+
 
 for i in range(p.get_device_count()):
     dev = p.get_device_info_by_index(i)
-    if (dev['name'] == 'Stereo Mix (Realtek(R) Audio)' and dev['hostApi'] == 0):
+    if (dev['name'][:10] == 'Stereo Mix'):
         dev_index = dev['index']
-        print('dev_index', dev_index)
+        print('SteroMix_index', dev_index)
 
 time.sleep(1000)
