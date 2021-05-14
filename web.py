176000d78cb281ccc75df8ccb8cfbd7c50c42d19
Gusb3ll <Kitpipat.0647@gmail.com>
2021-05-15 00:05:54 +0700

    Update web

--- web.py
+++ web.py
@@ -1,13 +1,27 @@
-import asyncio, json
-from quart import Quart, render_template
+from quart import Quart, render_template_string
 
 app = Quart(__name__)
 
 @app.route('/')
-async def index():
+async def test():
     with open('jp.txt', 'r', encoding='utf-8') as j:
         with open('en.txt', 'r', encoding='utf-8') as e:
-            return await render_template('index.html', title="Holotranslator", japantext=j.read(), englishtext=e.read())
+                return await render_template_string('''
+                    <!DOCTYPE html>
+                    <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                            <meta http-equiv="refresh" content="10">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>{{ title }}</title>
+                        </head>
+                        <body>
+                            <h2>{{ japantext }}</h2>
+                            <h2>{{ englishtext }}</h2>
+                        </body>
+                    </html>
+                ''', title="Holotranslator-test", japantext=j.read(), englishtext=e.read())
 
 if __name__ == '__main__':
     app.run(debug=True)
